6be8a46405533764a8771ca7964c7e77e33d7fb2
shiliyu <liyu.italy@hotmail.com>
2020-05-01 21:44:46 +0800

    Polish test

--- src/test/java/com/github/hcsp/ValidParenthesesTest.java
+++ src/test/java/com/github/hcsp/ValidParenthesesTest.java
@@ -2,15 +2,20 @@ package com.github.hcsp;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ValidParenthesesTest {
 
-    @Test
-    public void testIsValid() {
-        Assertions.assertTrue(ValidParentheses.isValid("()"));
-        Assertions.assertFalse(ValidParentheses.isValid("(]"));
-        Assertions.assertFalse(ValidParentheses.isValid("([)]"));
-        Assertions.assertTrue(ValidParentheses.isValid("{[]}"));
-        Assertions.assertTrue(ValidParentheses.isValid(""));
+    @ParameterizedTest(name = "{0} == {1}")
+    @CsvSource({
+            "true,  {}",
+            "false, {]",
+            "false, ([)]",
+            "true, {[]}",
+            "true,  ''"  // specify empty string by supplying two adjacent single-quotes
+    })
+    void testIsValid(boolean result, String str) {  // result type is boolean!
+        Assertions.assertEquals(result, ValidParentheses.isValid(str));
     }
 }
