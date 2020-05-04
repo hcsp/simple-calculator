ef191c3cef29fce4626d3f827a74cf3609a71ab7
Bo Zhang <zhangbodut@gmail.com>
2020-05-04 15:56:54 +0800

    Update ThreeSum15Test.java

--- src/test/java/com/github/hcsp/ThreeSum15Test.java
+++ src/test/java/com/github/hcsp/ThreeSum15Test.java
@@ -11,21 +11,29 @@ public class ThreeSum15Test {
 
     @Test
     public void testThreeSum1() {
-        Assertions.assertEquals(result, ThreeSum15.threeSum1(ints));
+        assertListEqualsIgnoringOrder(result, ThreeSum15.threeSum1(ints));
+    }
+    
+    private void assertListEqualsIgnoringOrder(List<List<Integer>> list1, List<List<Integer>> list2) {
+        Assertions.assertEquals(list1.size(), list2.size());
+        for(int i=0;i<list1.size();++i) {
+            Assertions.assertEquals(list1.get(0).stream().sorted().collect(toList()),
+                                   list2.get(0).stream().sorted().collect(toList()))
+        }
     }
 
     @Test
     public void testThreeSum2() {
-        Assertions.assertEquals(result, ThreeSum15.threeSum2(ints));
+        assertListEqualsIgnoringOrder(result, ThreeSum15.threeSum2(ints));
     }
 
     @Test
     public void testThreeSum3() {
-        Assertions.assertEquals(result, ThreeSum15.threeSum3(ints));
+        assertListEqualsIgnoringOrders(result, ThreeSum15.threeSum3(ints));
     }
 
     @Test
     public void testThreeSum4() {
-        Assertions.assertEquals(result, ThreeSum15.threeSum3(ints));
+        assertListEqualsIgnoringOrder(result, ThreeSum15.threeSum3(ints));
     }
 }
